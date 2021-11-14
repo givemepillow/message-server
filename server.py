c30ba67e51999e50c408723e51ec6f27c165eff2
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-11-14 03:16:44 +0300

    Update server. Added Clients and Dispatcher. Also created close function.

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 import selectors
 import socket
+from Dispatcher import Dispatcher
+from Clients import Clients
 
 
 class Server:
     __selector = selectors.DefaultSelector()
-    __BUFFER_SIZE = 1024 * 25
+    __BUFFER_SIZE = 1024 * 10
     __BACKLOG = 100
     __PORT = 6700
     __ADDRESS = '127.0.0.1'
 
     @classmethod
-    def _init(cls):
+    def __init(cls):
         sock = socket.socket()
         sock.bind((cls.__ADDRESS, cls.__PORT))
         sock.listen(cls.__BACKLOG)
@@ -19,6 +21,7 @@ class Server:
 
     @classmethod
     def start(cls):
+        cls.__init()
         while True:
             events = cls.__selector.select()
             for key, mask in events:
@@ -27,18 +30,31 @@ class Server:
 
     @classmethod
     def accept(cls, sock, mask):
-        conn, addr = sock.accept()  # Should be ready
-        print('accepted', conn, 'from', addr)
-        conn.setblocking(False)
-        cls.__selector.register(conn, selectors.EVENT_READ, cls.read)
+        connection, addr = sock.accept()  # Should be ready
+        print('accepted', connection, 'from', addr)
+        Clients.set_connection(connection, connection.fileno())  # save client socket
+        connection.setblocking(False)
+        cls.__selector.register(connection, selectors.EVENT_READ, cls.read)
 
     @classmethod
     def read(cls, connection, mask):
-        data = connection.recv(cls.__BUFFER_SIZE)  # Should be ready
-        if data:
-            print('echoing', repr(data), 'to', connection)
-            connection.send(data)  # Hope it won't block
-        else:
-            print('closing', connection)
-            cls.__selector.unregister(connection)
-            connection.close()
+        try:
+            data = connection.recv(cls.__BUFFER_SIZE)  # Should be ready
+            if data:
+                print('rec: ', repr(data), 'to', connection)
+                receiver, answer_data = Dispatcher.handle(data, connection.fileno())
+                for user_fd in Clients.get_user(receiver):
+                    Clients.connections[user_fd].send(answer_data)
+                connection.send(answer_data)
+            else:
+                print('closing', connection)
+                cls.close(connection)
+        except OSError as err:
+            print(err)
+            cls.close(connection)
+
+    @classmethod
+    def close(cls, connection):
+        Clients.remove_connection(connection.fileno())
+        connection.close()
+        cls.__selector.unregister(connection)
