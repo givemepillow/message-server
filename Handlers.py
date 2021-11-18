93450af8924d50f1937b107ac58ee8f25e099cab
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-11-18 14:17:49 +0300

    Init handler.

--- Handlers.py
+++ Handlers.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from DataTypes import Type
 
+from loader import storage
+
 
 class Handlers:
     @staticmethod
@@ -12,12 +14,18 @@ class Handlers:
         }
 
     @staticmethod
+    def init(request):
+        return request
+
+    @staticmethod
     def send_message(request):
         _from_id = request['from_id']
         _to_id = request['to_id']
-        request['date_time'] = datetime.now().strftime("%m-%d-%Y, %H:%M:%S")
+        request['date_time'] = datetime.now().timestamp() * 1000
+        print(request['date_time'])
         _message = request['message']
         # save message to db
+        storage.save_message(request['from_id'], request['to_id'], request['date_time'], request['message'])
         return request
 
     @staticmethod
